ec637f05bb82dc370b636b70edc44895f353d7bc
MironIrk <mironkct@gmail.com>
2023-03-14 22:42:24 +0800

    делаю логику передвижения плиток

---
+++ js/cell.js
@@ -0,0 +1,37 @@
+'use strict'
+
+export class Cell {
+  constructor(gridElement, x, y) {
+    const cell = document.createElement('div');
+    cell.classList.add('cell');
+    gridElement.append(cell);
+    this.x = x;
+    this.y = y;
+  }
+
+  linkTile(tile) {
+    tile.setXY(this.x, this.y);
+    this.linkedTile = tile;
+  }
+
+  unlinkTile() {
+    this.linkedTile = null;
+  }
+
+  isEmpty() {
+    return !this.linkedTile;
+  }
+
+  linkTileForMerge(tile) {
+    this.setXY(this.x, this.y);
+    this.linkedTileForMerge = tile;
+  }
+
+  hasTileForMerge() {
+    return !!this.linkedTileForMerge;
+  }
+
+  canAccept(newTile) {
+    return this.isEmpty() || (!this.hasTileForMerge() && this.linkedTile.value === newTile.value);
+  }
+}
